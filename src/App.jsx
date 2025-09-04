39b043c1713b659a50dc42c0cd563822e01f1e64
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-09-04 23:56:31 +0530

    setup the routing for my website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+// import { Provider } from "react-redux";
+// import appStore from "./utils/appStore";
+// import Feed from "./components/Feed";
+// import Connections from "./components/Connections";
+// import Requests from "./components/Requests";
+// import Premium from "./components/Premium";
+// import Chat from "./components/Chat";
 
 function App() {
-
   return (
     <>
-      <div>Hello WORLD</div>
+      {/* <Provider store={appStore}> */}
+        <BrowserRouter basename="/">  
+         {/* basename is where our application's root should point to  */}
+          <Routes>
+            <Route path="/" element={<AppLayout />}>  
+            {/* route matching our application's root */}
+              {/* <Route path="/" element={<Feed />} /> */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              {/* <Route path="/connections" element={<Connections />} /> */}
+              {/* <Route path="/requests" element={<Requests />} /> */}
+              {/* <Route path="/premium" element={<Premium />} /> */}
+              {/* <Route path="/chat/:targetUserId" element={<Chat />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      {/* </Provider> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
