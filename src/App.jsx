ff0da4e7c797e0749726684fa7bf226e1967e76a
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-20 01:39:02 +0530

    made changes in routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,36 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-// import { Provider } from "react-redux";
-// import appStore from "./utils/appStore";
-// import Feed from "./components/Feed";
-// import Connections from "./components/Connections";
-// import Requests from "./components/Requests";
+import { Provider } from "react-redux";
+import appStore from "./redux/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 // import Premium from "./components/Premium";
 // import Chat from "./components/Chat";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      {/* <Provider store={appStore}> */}
-        <BrowserRouter basename="/">  
-         {/* basename is where our application's root should point to  */}
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          {/* basename is where our application's root should point to  */}
           <Routes>
-            <Route path="/" element={<AppLayout />}>  
-            {/* route matching our application's root */}
-              {/* <Route path="/" element={<Feed />} /> */}
+            <Route path="/" element={<AppLayout />}>
+              {/* route matching our application's root */}
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
               <Route path="/profile" element={<Profile />} />
-              {/* <Route path="/connections" element={<Connections />} /> */}
-              {/* <Route path="/requests" element={<Requests />} /> */}
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
               {/* <Route path="/premium" element={<Premium />} /> */}
               {/* <Route path="/chat/:targetUserId" element={<Chat />} /> */}
             </Route>
           </Routes>
         </BrowserRouter>
-      {/* </Provider> */}
+      </Provider>
     </>
   );
 }
