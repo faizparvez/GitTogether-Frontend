bc7d9eb3b1d80aed0ddb7c0bd72da73b38a7310e
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-25 02:02:25 +0530

    created the send message func

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,65 +7,66 @@ import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-//   const [messages, setMessages] = useState([]);
-//   const [newMessage, setNewMessage] = useState("");
-//   const user = useSelector((store) => store.user);
-//   const userId = user?._id;
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
 
-//   const fetchChatMessages = async () => {
-//     const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
-//       withCredentials: true,
-//     });
+  //   const fetchChatMessages = async () => {
+  //     const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+  //       withCredentials: true,
+  //     });
 
-//     console.log(chat.data.messages);
+  //     console.log(chat.data.messages);
 
-//     const chatMessages = chat?.data?.messages.map((msg) => {
-//       const { senderId, text } = msg;
-//       return {
-//         firstName: senderId?.firstName,
-//         lastName: senderId?.lastName,
-//         text,
-//       };
-//     });
-//     setMessages(chatMessages);
-//   };
-//   useEffect(() => {
-//     fetchChatMessages();
-//   }, []);
+  //     const chatMessages = chat?.data?.messages.map((msg) => {
+  //       const { senderId, text } = msg;
+  //       return {
+  //         firstName: senderId?.firstName,
+  //         lastName: senderId?.lastName,
+  //         text,
+  //       };
+  //     });
+  //     setMessages(chatMessages);
+  //   };
+  //   useEffect(() => {
+  //     fetchChatMessages();
+  //   }, []);
 
   useEffect(() => {
     // if (!userId) {
     //   return;
     // }
     const socket = createSocketConnection();
-    // As soon as the page loaded, the socket connection is made and joinChat event is emitted
-    // socket.emit("joinChat", {
-    //   firstName: user.firstName,
-    //   userId,
-    //   targetUserId,
-    // });
+    // As soon as the page loads, the socket connection is made and joinChat event is emitted
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+    });
 
-//     socket.on("messageReceived", ({ firstName, lastName, text }) => {
-//       console.log(firstName + " :  " + text);
-//       setMessages((messages) => [...messages, { firstName, lastName, text }]);
-//     });
+    //     socket.on("messageReceived", ({ firstName, lastName, text }) => {
+    //       console.log(firstName + " :  " + text);
+    //       setMessages((messages) => [...messages, { firstName, lastName, text }]);
+    //     });
 
-//     return () => {
-//       socket.disconnect();
-//     };
+    // when the component unmounts
+    return () => {
+      socket.disconnect();
+    };
   }, [userId, targetUserId]);
 
-//   const sendMessage = () => {
-//     const socket = createSocketConnection();
-//     socket.emit("sendMessage", {
-//       firstName: user.firstName,
-//       lastName: user.lastName,
-//       userId,
-//       targetUserId,
-//       text: newMessage,
-//     });
-//     setNewMessage("");
-//   };
+    const sendMessage = () => {
+      const socket = createSocketConnection();
+      socket.emit("sendMessage", {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userId,
+        targetUserId,
+        text: newMessage,
+      });
+      setNewMessage("");
+    };
 
   return (
     <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
