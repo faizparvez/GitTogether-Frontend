97f2a054540af9befd69cfeec85527b24c3a8875
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-11-28 19:12:55 +0530

    created the hero component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Hero from './Hero';
 import Workflow from './Workflow';
 import Testimonials from './Testimonials';
+import ProductShowcase from './ProductShowcase';
 
 /**
  * The Home component serves as the main container for the landing page.
@@ -15,6 +16,7 @@ const Home = () => {
     // Using a React Fragment to group the components without adding extra nodes to the DOM.
     <>
       <Hero />
+      <ProductShowcase/>
       <Workflow />
       <Testimonials />
     </>
